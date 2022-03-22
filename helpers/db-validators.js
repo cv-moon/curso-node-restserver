182263e2c4a7abe4daf94716408dcada0b0297d9
cv-moon <cristian.chuquitarco92@gmail.com>
2022-03-22 09:36:34 -0500

    usuarios crud listo 22-3-2022

---
+++ helpers/db-validators.js
@@ -0,0 +1,29 @@
+const Role = require("../models/role");
+const Usuario = require("../models/usuario");
+
+const esRolValido = async (rol = "") => {
+  const existeRol = await Role.findOne({ rol });
+  if (!existeRol) {
+    throw new Error(`El rol ${rol} no está registrado`);
+  }
+};
+
+const emailExiste = async (correo = "") => {
+  const existe = await Usuario.findOne({ correo });
+  if (existe) {
+    throw new Error(`El correo ${correo} ya está registrado`);
+  }
+};
+
+const existeUsuarioPorId = async (id = "") => {
+  const existe = await Usuario.findById(id);
+  if (!existe) {
+    throw new Error(`El id: ${id} no existe`);
+  }
+};
+
+module.exports = {
+  esRolValido,
+  emailExiste,
+  existeUsuarioPorId,
+};
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,4 +1,5 @@
 const { Router } = require("express");
+const { check } = require("express-validator");
 const {
   usuariosGet,
   usuariosPost,
@@ -6,13 +7,51 @@ const {
   usuariosDelete,
   usuariosPatch,
 } = require("../controllers/usuarios");
+const { validarCampos } = require("../middlewares/validar-campos");
+const {
+  esRolValido,
+  emailExiste,
+  existeUsuarioPorId,
+} = require("../helpers/db-validators");
 
 const router = Router();
 
 router.get("/", usuariosGet);
-router.post("/", usuariosPost);
-router.put("/:id", usuariosPut);
-router.delete("/", usuariosDelete);
+router.post(
+  "/",
+  [
+    check("nombre", ' "El nombre es obligatorio').not().isEmpty(),
+    check(
+      "password",
+      ' "El password es obligatorio y más de 6 caracteres'
+    ).isLength({ min: 6 }),
+    check("correo", ' "El correo no es válido').isEmail(),
+    check("correo").custom(emailExiste),
+    // check("rol", ' "No es un rol permitido').isIn(["ADMIN_ROL", "USER_ROL"]),
+    check("rol").custom(esRolValido),
+    validarCampos,
+  ],
+  usuariosPost
+);
+router.put(
+  "/:id",
+  [
+    check("id", " No es un ID válido").isMongoId(),
+    check("id").custom(existeUsuarioPorId),
+    check("rol").custom(esRolValido),
+    validarCampos,
+  ],
+  usuariosPut
+);
+router.delete(
+  "/:id",
+  [
+    check("id", " No es un ID válido").isMongoId(),
+    check("id").custom(existeUsuarioPorId),
+    validarCampos,
+  ],
+  usuariosDelete
+);
 router.patch("/", usuariosPatch);
 
 // router.get("/", (req, res) => {
